527a4067b703c3ad27d0e1d90d1cdcc35fb0802c
Samyank ror <samyankror77@gmail.com>
2025-04-14 22:40:01 +0530

    pages and routes are configured

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import SignIn from "./pages/Signin";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/about' element={<About/>}    />
+          <Route path='/profile' element={<Profile/>} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
