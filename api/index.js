9a6e0165f88b695beb141b138dfd1d30c92fb4ef
Samyank ror <samyankror77@gmail.com>
2025-04-16 00:00:16 +0530

    database connected successfully

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
+const DbName='mern-estate'
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('connected to mongodb');
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
+    console.log('Server is running on port 3000');
 })
