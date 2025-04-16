5ebab48af6972f51961e5bc52ca755eca306ddb0
Samyank ror <samyankror77@gmail.com>
2025-04-16 17:04:15 +0530

    created signup controller and route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.routes.js'
+
 dotenv.config();
 
 const app = express();
+app.use(express.json())
+
 const DbName='mern-estate'
 mongoose.connect(process.env.MONGO)
 .then(()=>{
@@ -13,6 +18,12 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 })
 
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
+
+
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
+
