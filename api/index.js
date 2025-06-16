eb2f9938a6ffaacfb8069c973d66975961f87fe3
Samyank ror <samyankror77@gmail.com>
2025-06-17 00:24:11 +0530

    npm pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
 import uploadImageRouter from "./routes/upload.routes.js";
+import path from "path";
 
 dotenv.config();
 
@@ -23,10 +24,17 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve();
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 app.use("/api/upload", uploadImageRouter);
+
+app.use(express.static(path.join(__dirname,'client/dist')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
