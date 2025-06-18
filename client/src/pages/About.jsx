01803e30185651ae9578b1104a14f8d2906d31c2
Samyank ror <samyankror77@gmail.com>
2025-06-18 19:34:26 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,10 +5,10 @@ function About() {
     <>
       <div className="max-w-6xl mx-auto my-8 px-4">
         <h1 className="font-bold text-4xl p-3 text-slate-700">
-          About TrueRoof
+          About HomeScape
         </h1>
         <p className="mb-4 text-slate-700 mt-4">
-          At TrueRoof, we believe that finding the right property is more than just a transaction — it's about building a future. Whether you're searching for a home, an investment, or a place to grow your business, we’re here to guide you with clarity and confidence.
+          At HomeScape, we believe that finding the right property is more than just a transaction — it's about building a future. Whether you're searching for a home, an investment, or a place to grow your business, we’re here to guide you with clarity and confidence.
         </p>
         <p className="mb-4 text-slate-700">
            Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
